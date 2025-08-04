c94635c2d3c1f17bcf1206ce88a6c40e191dc5d2
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-08-04 20:50:42 -0300

    fix: ajuste nos cards de contas a pagar

--- apps/isomorphic/src/app/shared/accounts-payable/header-info-details.tsx
+++ apps/isomorphic/src/app/shared/accounts-payable/header-info-details.tsx
@@ -54,11 +54,17 @@ export const HeaderInfoDetails = () => {
 
    return (
       <div className="mx-auto mb-4 flex max-w-full items-start rounded-lg bg-[#CEBDA3] py-4 shadow-md">
-         <div className="grid w-full grid-cols-1 gap-4 px-4 md:grid-cols-4">
+         <div className="grid w-full grid-cols-1 gap-4 px-4 md:grid-cols-5">
             <div className="flex flex-col items-center justify-center rounded-lg bg-gray-50 p-3">
                <span className="mb-2 text-sm text-gray-600">TOTAL A PAGAR</span>
                <div className="text-3xl font-semibold text-[#17345F]">
                   {totals ? formatCurrency(totals.totalPayable) : 'R$ 0,00'}
+               </div>
+            </div>
+            <div className="flex flex-col items-center justify-center rounded-lg bg-gray-50 p-3">
+               <span className="mb-2 text-sm text-gray-600">TOTAL VENCIDO</span>
+               <div className="text-3xl font-semibold text-[#17345F]">
+                  {totals ? formatCurrency(totals.totalOverdue) : 'R$ 0,00'}
                </div>
             </div>
             <div className="flex flex-col items-center justify-center rounded-lg bg-gray-50 p-3">
